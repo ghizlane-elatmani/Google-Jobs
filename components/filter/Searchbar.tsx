6684f6e67cc34d29b98eb15d05b958fc9f474606
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-05-01 09:44:43 +0100

    1.1.2: GET request w/ axios (Google Jobs API)

--- components/filter/Searchbar.tsx
+++ components/filter/Searchbar.tsx
@@ -13,15 +13,15 @@ const Searchbar = () => {
       }}
     >
       <form className="bg-blue-50 max-w-3xl m-auto p-1 flex gap-2 justify-between rounded-sm">
-        <div className="bg-blue-50 w-full px-2 flex items-center gap-1 rounded-sm">
-          <BiBriefcaseAlt />
+        <div className="bg-blue-50 w-full px-2 flex items-center gap-2 rounded-sm">
+          <BiBriefcaseAlt className="text-neutral-400" />
           <input
             type="text"
             placeholder="Title, companies, expertise or benefits"
-            className="w-full bg-blue-50 outline-none text-xs sm:text-base"
+            className="w-full bg-blue-50 outline-none text-xs sm:text-sm placeholder:text-neutral-400"
           />
         </div>
-        <button className="bg-blue-600 text-blue-50 px-6 py-2 rounded-sm text-xs sm:text-base">
+        <button className="bg-blue-600 text-blue-50 px-6 md:px-10 py-2 rounded-sm text-xs sm:text-sm">
           Search
         </button>
       </form>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,44 @@
-import { Fragment } from "react";
+import { useState, Fragment } from "react";
 import Head from "next/head";
 import Navbar from "@/components/layout/Navbar";
 import Searchbar from "@/components/filter/Searchbar";
 import Layout from "@/components/layout/Layout";
 import Filter from "@/components/filter/Filter";
 import JobList from "@/components/jobs/JobList";
+import axios from "axios";
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+
+  const baseUrl = `https://serpapi.com/search.json?engine=google_jobs&q=${searchQuery}&google_domain=google.com&api_key=${process.env.API_KEY}`;
+
   return (
     <Fragment>
-      {/* ===== HEAD ===== */}
       <Head>
         <title>Google Jobs | Search for your dream job</title>
       </Head>
 
-      {/* ===== NAVBAR & SEARCH BAR ===== */}
       <Navbar />
       <Searchbar />
 
-      {/* ===== FILTER ===== */}
       <Layout>
         <Filter />
         <JobList />
       </Layout>
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const startupURL = `https://serpapi.com/search.json?engine=google_jobs&q=Developer&google_domain=google.com&api_key=${process.env.API_KEY}`;
+
+  const response = await axios.get(startupURL);
+  const data = await response.data;
+
+  return {
+    props: {
+      jobs: data,
+    },
+  };
 }
